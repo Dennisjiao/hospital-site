5bcebdde4adec631f27a81707ce84955e0cab5b5
Dennisjiao <70896385+Dennisjiao@users.noreply.github.com>
2022-06-26 19:28:03 +0100

    前端页面显示，有一个取值问题还没解决

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,20 @@
 <template>
-  <div>
-    <nuxt/>
+  <div class="app-container">
+    <div id="main">
+      <!-- 公共头 -->
+      <myheader/>
+      <div class="main-container">
+        <el-scrollbar class='page-component__scroll'>
+        <!-- 内容区域 -->
+        <nuxt/>
+        </el-scrollbar>
+      </div>
+        <!-- 公共底 -->
+      <myfooter/>
+    </div>
   </div>
 </template>
+
 
 <script>
 import '~/assets/css/app.css'
